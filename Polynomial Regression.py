fd6798d260cb9981d567a87b07cdea5eafedb233
rishitoshsingh <rishitoshs@gmail.com>
2018-10-26 21:45:56 +0530

    Initial Commit

---
+++ Polynomial Regression.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 21:42:51 2018
+
+@author: rishi
+"""
+
---
+++ Simple Linear Regression.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 14:02:40 2018
+
+@author: rishi
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset 
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,1].values
+
+# Spliting the dataset into Tranining Set and Test Set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Feature Scaling 
+
+
+# Fitting Simple Linear Regression to Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X = X_train, y = y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualizing the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+
+# Visualizing the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
